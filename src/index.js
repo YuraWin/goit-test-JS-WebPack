78a2e04c921e03f66c4baf6beb8d5dd1801e3851
YuraWin <YuraWin@ukr.net>
2020-10-23 16:39:04 +0300

    Commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './styles.css';
+
+const logMessage = (time) => {
+    console.log(`Лог при вызове  calback-функции через ${time} мсек`);
+}
+
+console.log('До вызова Settimeout');
+
+setTimeout(() => { logMessage() }, 2000, 2000);
+
+console.log('После вызова SetTimeout');
