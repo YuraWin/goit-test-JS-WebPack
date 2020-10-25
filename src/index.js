d22377b55b67fd94d1afd2c4d09617eff549a145
YuraWin <yurawin@ukr.net>
2020-10-25 23:42:11 +0300

    Home module 11

--- src/index.js
+++ src/index.js
@@ -1,11 +1,81 @@
 import './styles.css';
 
-const logMessage = (time) => {
-    console.log(`Лог при вызове  calback-функции через ${time} мсек`);
-}
+// **module 11   1*******************************************************
+// const logMessage = (time) => {
+//     console.log(`Лог при вызове  calback-функции каждые ${time} мсек - ${Date.now()}`);
+// }
 
-console.log('До вызова Settimeout');
+// console.log('До вызова SetTimeout');
 
-setTimeout(() => { logMessage() }, 2000, 2000);
+// const timerId = setInterval(logMessage, 2000, 2000);
+// const timerId2 = setTimeout((y, x) => {
+//     console.log(`Расчёт через 3сек ${x}+${y} = ${x + y}`)
+// }, 3000, 10, 15);
 
-console.log('После вызова SetTimeout');
+// console.log('После вызова SetTimeout');
+
+// clearTimeout(timerId2);
+// clearTimeout(timerId);
+
+// const intervalId = setInterval(logMessage, 2000, 2000);
+// const shouldCancelInterval = Math.random() > 0.3;
+// console.log(shouldCancelInterval);
+
+// if (shouldCancelInterval) {
+//     clearInterval(intervalId);
+// }
+
+// const timeoutId = setTimeout(() => {
+//     clearInterval(intervalId)
+//     console.log('stop intervalId')
+// }, 10000);
+
+// **module 11   2*******************************************************
+
+// const NOTIFICATION_DELAY = 3000;
+// let timeoutId = null;
+
+// const refs = {
+//     notification: document.querySelector('.js-alert'),
+// }
+
+// refs.notification.addEventListener('click', onNotificationClick);
+
+// showNotification();
+
+// function onNotificationClick() {
+//     hideNotification();
+//     clearInterval(timeoutId);
+// }
+
+// function showNotification() {
+//     refs.notification.classList.add('is-visible');
+//     timeoutId = setTimeout(() => {
+//         console.log('Закрываем алерт чтобы не висел');
+//         hideNotification();
+
+//     }, 3000)
+// }
+// function hideNotification() {
+//     refs.notification.classList.remove("is-visible")
+// }
+
+// **module 11   3***********************************************************************
+const PROMPT_DELAY = 1000;
+const MAX_PROMPT_ATTEMPTS = 3;
+let intervalId = null;
+
+let promptCounter = 0;
+let hasSubcribed = false;
+
+intervalId = setInterval(() => {
+    if (promptCounter === MAX_PROMPT_ATTEMPTS || hasSubcribed) {
+        console.log('Останавливаем интервал');
+        clearInterval(intervalId);
+        return;
+    }
+
+    console.log('Подпишись на рассылку', Date.now());
+    promptCounter += 1;
+}, PROMPT_DELAY);
+// ***********************************************************************
